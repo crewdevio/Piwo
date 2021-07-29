f09307748574808a11d9b2f2fc1ef57ca5786d0f
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-07-29 11:30:28 -0400

    refactor: colors

--- utils/colors.ts
+++ utils/colors.ts
@@ -8,27 +8,19 @@
 
 import { rgb24 } from "fmt/colors.ts";
 
-interface RGB {
-	r: number;
-	g: number;
-	b: number;
-}
-
-export const red = coloredText(rgbParser(255, 150, 186));
-export const purple = coloredText(rgbParser(168, 130, 255));
-export const blue = coloredText(rgbParser(171, 202, 255));
-export const yellow = coloredText(rgbParser(255, 211, 115));
-export const orange = coloredText(rgbParser(235, 186, 120));
+export const red = coloredText(255, 150, 186);
+export const purple = coloredText(168, 130, 255);
+export const blue = coloredText(171, 202, 255);
+export const yellow = coloredText(255, 211, 115);
+export const orange = coloredText(235, 186, 120);
 
 export const keyColor = purple;
 export const stringColor = blue;
 export const nullColor = red;
 export const numberAndBoolColor = yellow;
 
-function coloredText(color: RGB) {
-	return (text: string | number) => rgb24(String(text), color);
-}
-
-function rgbParser(...rgb: number[]) {
-	return { r: rgb[0], g: rgb[1], b: rgb[2] };
+function coloredText(...color: number[]) {
+	const [ r, g, b] = color;
+	const rgb = { r, g, b };
+	return (text: string | number) => rgb24(String(text), rgb);
 }
