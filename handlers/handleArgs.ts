b6c060ab5ae62c05f4740a6d5fbe9695d1b0ea67
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-07-29 13:57:58 -0400

    fix: --form flag

--- handlers/handleArgs.ts
+++ handlers/handleArgs.ts
@@ -12,15 +12,17 @@ import { Args } from "flags/mod.ts";
 
 function handleArgs(args: Args) {
 	const firstArg = args._[0];
+	const form = args.f || args.form;
 	let method: Method;
 	let url = args._[1] as string;
 	let body = undefined;
 
-	if (firstArg === "GET" || firstArg === "POST" || firstArg === "PUT" || firstArg === "DELETE" || firstArg === "PATCH" || args.form || args.f) {
-		if (args.form || args.f) {
-			body = inputToForm(args._.slice(1) as string[])
-			method = args.form || args.f;
-			url = args._[0] as string;
+	if (firstArg === "GET" || firstArg === "POST" || firstArg === "PUT" || firstArg === "DELETE" || firstArg === "PATCH" || form) {
+		if (form) {
+			const isForm = args.form;
+			method = args.f || firstArg;
+			body = inputToForm(args._.slice(isForm ? 2 : 1) as string[])
+			url = args._[isForm ? 1 : 0] as string;
 		} else {
 			body = inputToJSON(args._.slice(2) as string[]);
 			method = firstArg as Method;
@@ -37,7 +39,7 @@ function handleArgs(args: Args) {
 		flags: {
 			help: args.help || args.h,
 			version: args.version || args.v,
-			form: args.form || args.f
+			form
 		}
 	}
 }
