970aabeac96f2dcfaa66247653dd3da9ae2e919e
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-09-12 13:31:56 -0400

    add test for unexpected inputs

---
+++ tests/unexpectedArgs.test.ts
@@ -0,0 +1,54 @@
+import { Merlin } from "merlin";
+import { validateArgs } from "../utils/args/validate.ts";
+import { purple, red, yellow } from "../utils/color/colors.ts";
+import type { Args } from "../types.ts";
+
+const test = new Merlin();
+
+test.assertEqual("version", {
+  expect() {
+    const obj: Args = {
+      flags: { version: true },
+      url: "ajio.com",
+      method: "GET",
+    };
+    return validateArgs(obj as Required<Args>);
+  },
+  toBe() {
+    return {
+      msg: `the flag ${purple("version")} doesn't need arguments`,
+      exit: false,
+      type: `${yellow("warn")}`,
+    };
+  },
+});
+
+test.assertEqual("form", {
+  expect() {
+    const obj: Args = { flags: { form: true } };
+    return validateArgs(obj as Required<Args>);
+  },
+  toBe() {
+    return {
+      msg: `the flag ${purple("form")} needs the following arguments: ${
+        yellow("[METHOD]")
+      }, ${yellow("[URL]")}, ${yellow("[BODY]")}`,
+      exit: true,
+      type: `${red("error")}`,
+    };
+  },
+});
+
+test.assertEqual("request", {
+  expect() {
+    const obj: Args = { method: "POST" }
+    return validateArgs(obj as Required<Args>);
+  },
+  toBe() {
+    return {
+      msg: `miss ${yellow("[URL]")}`,
+      exit: true,
+      type: `${red("error")}`,
+    };
+  },
+});
