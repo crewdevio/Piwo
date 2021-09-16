e799c42f05fe120f463d304bf97f53149a8262de
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-09-16 12:11:36 -0400

    refactor: run command for test

--- utils/readJson.ts
+++ utils/readJson.ts
@@ -1,3 +1,6 @@
+import { red, yellow } from "./color/colors.ts";
+import { runJson } from "../types.ts";
+
 export async function readJson(filePath: string): Promise<unknown> {
   const decoder = new TextDecoder("utf-8");
 
@@ -9,4 +12,19 @@ export async function readJson(filePath: string): Promise<unknown> {
     err.message = `${filePath}: ${err.message}`;
     throw err;
   }
+}
+
+export async function getRequest(alias: string): Promise<Request> {
+  const json = await readJson("./request.json") as runJson;
+  const request = json[alias];
+  if (!request) {
+    console.error(
+      `${red("error")}: ${yellow(alias)} alias not found in ${
+        yellow("request.json")
+      }`,
+    );
+    Deno.exit();
+  }
+
+  return request;
 }
