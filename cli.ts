0ee645c068c75da472b92e96ea738997eeebf9b9
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-09-16 12:11:36 -0400

    refactor: run command for test

--- cli.ts
+++ cli.ts
@@ -12,9 +12,7 @@ import parse from "./utils/args/parser.ts";
 import output from "./utils/output/output.ts";
 import { customFetch, runFetch } from "./utils/customFetch.ts";
 import { validateArgs } from "./utils/args/validate.ts";
-import { red, yellow } from "./utils/color/colors.ts";
-import { runJson } from "./types.ts";
-import { readJson } from "./utils/readJson.ts";
+import { getRequest } from "./utils/readJson.ts";
 
 const denoArgs = Deno.args;
 const args = parse(denoArgs);
@@ -31,18 +29,9 @@ if (args) {
   const { flags, command } = args;
 
   if (command) {
-    const request = await readJson("./request.json") as runJson;
-    const alias = request[command.split(" ")[1]];
-    if (!alias) {
-      console.error(
-        `${red("error")}: ${yellow(alias)} alias not found in ${
-          yellow("request.json")
-        }`,
-      );
-      Deno.exit();
-    }
-
-    output(await runFetch(alias.url, alias));
+    const alias = command.split(" ")[1];
+    const request = await getRequest(alias);
+    output(await runFetch(request.url, request));
   } else if (flags?.help) {
     console.log(helpCommand);
   } else if (flags?.version) {
