ea5921456e710ef47c0299495944c256accf90f0
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-09-17 12:50:51 -0400

    feat(run): error message when request file not found

--- utils/readJson.ts
+++ utils/readJson.ts
@@ -1,12 +1,17 @@
 import { red, yellow } from "./color/colors.ts";
 import { formData } from "./formData.ts";
+import { exists } from "fs/mod.ts";
 
 export async function readJson(filePath: string) {
-  const decoder = new TextDecoder("utf-8");
-
-  const content = decoder.decode(await Deno.readFile(filePath));
+  if (!await exists(filePath)) {
+    console.error(`${red("error")}: ${yellow(filePath)} not found`);
+    Deno.exit();
+  }
 
   try {
+    const decoder = new TextDecoder("utf-8");
+    const content = decoder.decode(await Deno.readFile(filePath));
+
     return JSON.parse(content);
   } catch (err) {
     err.message = `${filePath}: ${err.message}`;
