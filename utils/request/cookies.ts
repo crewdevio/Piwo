0224c10c455ad066edb6473e3d276b62be201aad
ghaerdi <ghaerdi@outlook.com>
2022-02-02 21:56:45 -0400

    feat: use localStorage to save cookies

--- utils/request/cookies.ts
+++ utils/request/cookies.ts
@@ -1,43 +1,21 @@
-import { join } from "path/mod.ts";
-import * as fs from "fs/mod.ts";
-import { cookiePath, storagePath } from "../../info.ts";
-
-const { exists } = fs;
-const { mkdir, writeFile, readFile } = Deno;
-
 export function saveCookie(headers: Headers, url: string) {
-  [storagePath, cookiePath].forEach(
-    async (dir) => !(await exists(dir)) && (await mkdir(dir)),
-  );
-
-  const filePath = join(cookiePath, getURL(url));
-  const encoder = new TextEncoder();
-
-  ["set-cookie", "cookie"].forEach((v) => {
-    if (headers.has(v)) {
-      const cookie = headers.get(v) as string;
-      const data = encoder.encode(cookie);
-      writeFile(filePath, data);
-    }
-  });
-}
-
-export async function getCookie(url: string) {
-  const path = join(cookiePath, getURL(url));
-  if (await exists(path)) {
-    const decoder = new TextDecoder("utf-8");
-    const data = await readFile(path);
-
-    return decoder.decode(data);
+  if (headers.has("set-cookie")) {
+    const cookie = headers.get("set-cookie") as string;
+    localStorage.setItem(getHostname(url), cookie);
   }
 }
 
-function getURL(url: string) {
-  const match = url.match(/[A-z0-9:.]+/g);
-  const len = match?.length;
-  return len === 1
-    ? match?.[0]?.startsWith("www.") ? match?.slice(4) : match?.[0]
-    : match?.[0].startsWith("http")
-    ? match?.[1]
-    : match?.[0];
+export function getCookie(url: string) {
+  return localStorage.getItem(getHostname(url));
+}
+
+function getHostname(url: string) {
+  if (url.startsWith("www.")) {
+    url = `https://${url.slice(4)}`
+  } else if (!url.startsWith("http")) {
+    url = `https://${url}`
+  }
+
+  const { hostname } = new URL(url);
+  return hostname;
 }
